457ab29edadf747354bba9de77e0ff6ffe05f385
hiago <hiagop22@gmail.com>
2020-10-25 12:51:27 -0300

    Implementado o TestesUnitáriosEntidades.h

--- Trabalho2/include/TestesUnitariosEntidades.h
+++ Trabalho2/include/TestesUnitariosEntidades.h
@@ -1,6 +1,6 @@
 #ifndef TESTESUNITARIOSENTIDADES_H
 #define TESTESUNITARIOSENTIDADES_H
-
+#include "Dominios.h"
 
 class TesteUnitarioEntidade{
 protected:
@@ -18,6 +18,14 @@ public:
 
 class TesteUnitarioAplicacao: public TesteUnitarioEntidade{
 private:
+    CodigoAplicacao* codigo;
+    ValorDeAplicacao* valor;
+    Data* data;
+
+    const string CODIGO_APLICACAO_VALIDO;
+    const int VALOR_APLICACAO_VALIDO = 500;
+    const string DATA_VALIDA;
+
     void setUp();
     void tearDown();
     void testarCenarioSucesso();
@@ -27,6 +35,14 @@ public:
 
 class TesteUnitarioConta: public TesteUnitarioEntidade{
 private:
+    CodigoBanco* banco;
+    CodigoAgencia* agencia;
+    Numero* numero;
+
+    const string CODIGO_BANCO_VALIDO;
+    const string CODIGO_AGENCIA_VALIDO;
+    const string NUMERO_VALIDO;
+
     void setUp();
     void tearDown();
     void testarCenarioSucesso();
@@ -36,6 +52,24 @@ public:
 
 class TesteUnitarioProduto: public TesteUnitarioEntidade{
 private:
+    CodigoProduto* codigo;
+    Classe* classe;
+    Emissor* emissor;
+    Prazo* prazo;
+    Data* vencimento;
+    Taxa* taxa;
+    Horario* horario;
+    ValorMinimo* valor;
+
+    const string CODIGO_PRODUTO_VALIDO;
+    const string NOME_CLASSE_VALIDO;
+    const string NOME_EMISSOR_VALIDO;
+    const int PRAZO_VALIDO = 24;
+    const string DATA_VENCIMENTO_VALIDO;
+    const int TAXA_VALIDA = 15;
+    const string HORARIO_VALIDO;
+    const int VALOR_MINIMO_VALIDO = 100;
+
     void setUp();
     void tearDown();
     void testarCenarioSucesso();
@@ -45,6 +79,18 @@ public:
 
 class TesteUnitarioUsuario: public TesteUnitarioEntidade{
 private:
+    Nome* nome;
+    Endereco* endereco;
+    Cep* cep;
+    Cpf* cpf;
+    Senha* senha;
+
+    const string NOME_VALIDO;
+    const string ENDERECO_VALIDO;
+    const int CEP_VALIDO = 1111111;
+    const string CPF_VALIDO;
+    const string SENHA_VALIDA;
+
     void setUp();
     void tearDown();
     void testarCenarioSucesso();
